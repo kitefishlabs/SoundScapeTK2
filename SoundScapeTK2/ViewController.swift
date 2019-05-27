25472362bfb64a1d3be5bd899ecfe95b4d51ac9f
Tom Stoll <tom@kitefishlabs.com>
2019-05-27 16:36:47 -0400

    Initial Commit

---
+++ SoundScapeTK2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundScapeTK2
+//
+//  Created by kfl on 5/27/19.
+//  Copyright © 2019 kfl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SoundScapeTK2Tests/SoundScapeTK2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  SoundScapeTK2Tests.swift
+//  SoundScapeTK2Tests
+//
+//  Created by kfl on 5/27/19.
+//  Copyright © 2019 kfl. All rights reserved.
+//
+
+import XCTest
+@testable import SoundScapeTK2
+
+class SoundScapeTK2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
